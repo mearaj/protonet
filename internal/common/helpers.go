684ac74d69b9ca9d7ef883bcfc2e4f0f10550fc7
Mearaj Bhagad <mearajbhagad@gmail.com>
2024-01-13 08:46:58 +0530

    use go version below 1.21a and minor fix applied

--- internal/common/helpers.go
+++ internal/common/helpers.go
@@ -476,10 +476,11 @@ func GetEthAddress(privateKeyStr string) (string, error) {
 	if privateKeyStr == "" {
 		return "", errors.New("private key is empty")
 	}
-	privateKey, err := crypto.HexToECDSA(privateKeyStr)
+	bs, err := hex.DecodeString(privateKeyStr)
 	if err != nil {
 		return "", err
 	}
+	privateKey := crypto.ToECDSAUnsafe(bs)
 	publicKey := privateKey.Public()
 	publicKeyECDSA, ok := publicKey.(*ecdsa.PublicKey)
 	if !ok {
